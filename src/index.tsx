a35267b569ffc82c4f012a5b59d1965f4eed4bf6
mxy323 <maxinyue323@163.com>
2022-09-22 22:39:42 +0800

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/page.less";
-//import "./styles/style.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
